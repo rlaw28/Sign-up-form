4bd46689f87c1800d1ff2a6cd85769b2703327d4
rlaw28 <rashaunlawson28@gmail.com>
2023-03-27 03:10:31 -0400

    Add script file and create variables for form element and password inputs.

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('main-form');
+const password = document.getElementById('password');
+const confirmPass = document.getElementById('confirm-pass')
+
+
+form.addEventListener('submit', () => {
+    
+})
